6d2fa1966a013712f06c5d2e10f44d4f7feb3335
ZoeGerst <81997340+ZoeGerst@users.noreply.github.com>
2021-04-15 16:59:28 -0700

    Add files via upload

--- calc.py
+++ calc.py
@@ -1,1 +1,11 @@
 
+def calc(a, b):
+    sum = a + b
+    print(sum)
+    sub = a - b
+    print(sub)
+    mul = a * b
+    print(mul)
+    divide = a / b
+    print(divide)
+    list = [sum, sub, mul, divide]
