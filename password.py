ade6539e90e6b2cbf412d7121e7e92c3677fe896
ZoeGerst <81997340+ZoeGerst@users.noreply.github.com>
2021-04-16 20:25:15 -0700

    Add files via upload

--- password.py
+++ password.py
@@ -1,9 +1,17 @@
 
 import random
+import string
 
 def password():
 
     length = input("Length of password: ")
-    letters = 'abcdefghijklmnopqrstuvwxyz'
-    generate = ''.join(random.choices(letters, k=(length)))
+    LETTERS = string.ascii_letters
+    generate = f'{LETTERS}'
+    generate = list(generate)
+    random.shuffle(generate)
+    new_password = random.choices(generate, k = length)
+    new_password = ''.join(new_password)
 
+    return new_password
+
+print(password())
