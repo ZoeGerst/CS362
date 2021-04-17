79eccdda93cebea9678d8c566ee29df4e9eebdd3
ZoeGerst <81997340+ZoeGerst@users.noreply.github.com>
2021-04-16 19:19:07 -0700

    Add files via upload

--- calc.py
+++ calc.py
@@ -1,11 +1,38 @@
 
-def calc(a, b):
-    sum = a + b
-    print(sum)
-    sub = a - b
-    print(sub)
-    mul = a * b
-    print(mul)
-    divide = a / b
-    print(divide)
-    list = [sum, sub, mul, divide]
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+while True:
+    choice = input("Enter choice(1, 2, 3, or 4): ")
+    if choice in ('1', '2', '3', '4'):
+        first = float(input("Enter first number: "))
+        second = float(input("Enter second number: "))
+
+        if choice == '1':
+            print(first, "+", second, "=", add(first, second))
+
+        elif choice == '2':
+            print(first, "-", second, "=", subtract(first, second))
+
+        elif choice == '3':
+            print(first, "*", second, "=", multiply(first, second))
+
+        elif choice == '4':
+            print(first, "/", second, "=", divide(first, second))
+        break
+    
