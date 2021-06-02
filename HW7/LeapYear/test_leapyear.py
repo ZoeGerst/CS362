b306ff210afeddb6a6b89cb348704e41fc7d9aa3
ZoeGerst <81997340+ZoeGerst@users.noreply.github.com>
2021-06-02 16:46:56 -0700

    Add files via upload
    Second leap year test, test to make sure regular years print "is not a leap year"

--- HW7/LeapYear/test_leapyear.py
+++ HW7/LeapYear/test_leapyear.py
@@ -6,6 +6,9 @@ class TestCase(unittest.TestCase):
     def test_is(self):
         self.assertEqual(leapyear.check(2020), "is a leap year")
 
+    def test_is(self):
+        self.assertEqual(leapyear.check(2022), "is not a leap year")
+
 
 if __name__ == '__main__':
     unittest.main()
