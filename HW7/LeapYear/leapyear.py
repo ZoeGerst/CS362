b0af623a05e3360e0007dbf0d83c5076d1f124aa
ZoeGerst <81997340+ZoeGerst@users.noreply.github.com>
2021-06-02 14:45:36 -0700

    Add files via upload
    Program upload

---
+++ HW7/LeapYear/leapyear.py
@@ -0,0 +1,21 @@
+def check(year):
+
+    if(year % 4) == 0:
+
+        if(year % 100) == 0:
+
+            if(year % 400) == 0:
+
+                return "is a leap year"
+
+            else:
+
+                return "is not a leap year"
+
+        else:
+
+            return "is a leap year"
+
+    else:
+
+        return "is not a leap year"
